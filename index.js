77d37922aedc9e23e1defaa074c0888f11712a09
wangbo2013 <493800661@qq.com>
2025-10-04 18:06:04 +0800

    修改

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const formatToFixed = (money, decimals = 2) => {
     ).toFixed(decimals);
   }
 const Format = {
-  // 格式化金额展示： 12341234.246 -> $ 12,341,234.25
+  // 格式化金额展示： 12341234.246 -> $12,341,234.25
   formatMoney: (money, symbol = "", decimals = 2) =>
     formatToFixed(money, decimals)
       .replace(/\B(?=(\d{3})+\b)/g, ",")
